6b880c5f421f3a3c55131f63e9b2096dd498c2eb
Eric Chen <echen606@usc.edu>
2023-02-28 22:05:22 -0800

    update

--- grove_sensors.py
+++ grove_sensors.py
@@ -2,10 +2,11 @@ from grove_rgb_lcd import *
 
 prev_threshold = 0
 current_threshold = -1
+nl = '\n'
 setRGB(0,128,64)
 
 while True:
     if current_threshold != prev_threshold:
         current_threshold = prev_threshold
-        setText(f"{\n}{current_threshold:3}cm")
+        setText(f"{nl}{current_threshold:3}cm")
     
