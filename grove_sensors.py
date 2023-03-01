69a3cb5f2ba7216b02972e3672e8d1c08f91b3b8
Eric Chen <echen606@usc.edu>
2023-02-28 21:59:24 -0800

    update

--- grove_sensors.py
+++ grove_sensors.py
@@ -7,5 +7,5 @@ setRGB(0,128,64)
 while True:
     if current_threshold != prev_threshold:
         current_threshold = prev_threshold
-        setText(f"{threshold}cm")
+        setText(f"{current_threshold}cm")
     
